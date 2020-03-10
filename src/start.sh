585ae5a5b9fb0992c6a61ec0ef833ba8c8b860cd
Olumide Omotoso <olumide@bytelabs.ng>
2020-03-10 09:55:08 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,14 +1,14 @@
 #!/bin/ash
 
 #accept and forward v4/v6 via NAT
-sudo iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -m policy --dir out --pol ipsec -j ACCEPT
-sudo iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -j MASQUERADE
-sudo ip6tables -t nat -A POSTROUTING -s fec0::/120 -m policy --dir out --pol ipsec -j ACCEPT
-sudo ip6tables -t nat -A POSTROUTING -s fec0::/120 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -m policy --dir out --pol ipsec -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -j MASQUERADE
+ip6tables -t nat -A POSTROUTING -s fec0::/120 -m policy --dir out --pol ipsec -j ACCEPT
+ip6tables -t nat -A POSTROUTING -s fec0::/120 -j MASQUERADE
 
 #allow forwarding
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl -w net.ipv6.conf.all.forwarding=1
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
 
 #start vpn server
 ipsec start --nofork
